b59c3ae28de76c4dc84fb35d5d996b348ea24fb0
Sakulomodrep <lucas_perdomo_99@hotmail.com>
2022-10-26 21:07:29 -0300

    atividade 1 logica de programacao

---
+++ Front end_12_Logica de programação atividade 01.js
@@ -0,0 +1,22 @@
+let peso = 50
+if (peso<100){
+    console.log("A peça deve ter no minimo 100g")
+}else{
+    console.log("A peça posui o tamanho adequado")
+}
+
+var listaDePeças = ["Lanterna", "Motor", "Amor_tece_dor", "cambio", "painel", "cabeçote"] 
+if(listaDePeças.length < 10){
+    //é possivel cadstrar
+    console.log("É possivel cadastrar mais")
+}else{
+    console.log("Não é possivel cadastrar mais peças")
+}
+
+let nomePeça = "Peça"
+
+if(nomePeça.length >3){
+    console.log("Peça tem caracteres necessários")
+}else{
+    console.log("peça nao tem caracteres necessários")
+}
